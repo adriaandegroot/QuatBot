e5707070118d7e477382bfed85f61a64d64cbe8b
Adriaan de Groot <groot@kde.org>
2019-05-15 09:22:55 +0200

    quatbot: Initial checkin

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+ *  SPDX-License-Identifier: BSD-2-Clause
+ *  SPDX-License-File: LICENSE
+ *
+ * Copyright 2019 Adriaan de Groot <groot@kde.org>
+ */   
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
