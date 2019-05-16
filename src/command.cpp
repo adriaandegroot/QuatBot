f7c5593883bb005454a59e6e3a4bc0a9ecc9d4a0
Adriaan de Groot <groot@kde.org>
2019-05-16 13:43:21 +0200

    command: add op and deop commands for operators management

--- src/command.cpp
+++ src/command.cpp
@@ -7,6 +7,8 @@
 
 #include "command.h"
 
+#include "quatbot.h"
+
 #include <room.h>
 
 #include <QProcess>
@@ -44,9 +46,55 @@ QString BasicCommands::moduleName() const
 void BasicCommands::handleCommand(QMatrixClient::Room* room, const CommandArgs& l)
 {
     if (l.command == QStringLiteral("echo"))
+    {
         message(room, l.args);
+    }
     else if (l.command == QStringLiteral("fortune"))
+    {
         fortune(room);
+    }
+    else if (l.command == QStringLiteral("op"))
+    {
+        if (m_bot->checkOps(l, room))
+        {
+            if (l.args.count() == 1) 
+            {
+                if (m_bot->setOps(l.args.first(), true))
+                {
+                    message(room, QString("%1 is now an operator").arg(l.args.first()));
+                }
+                else
+                {
+                    message(room, QString("%1 failed.").arg(displayCommand("op")));
+                }
+            }
+            else
+            {
+                message(room, QString("Usage: %1 <userid>").arg(displayCommand("op")));
+            }
+        }
+    }
+    else if (l.command == QStringLiteral("deop"))
+    {
+        if (m_bot->checkOps(l, room))
+        {
+            if (l.args.count() == 1) 
+            {
+                if (m_bot->setOps(l.args.first(), false))
+                {
+                    message(room, QString("%1 is no longer an operator").arg(l.args.first()));
+                }
+                else
+                {
+                    message(room, QString("%1 failed.").arg(displayCommand("deop")));
+                }
+            }
+            else
+            {
+                message(room, QString("Usage: %1 <userid>").arg(displayCommand("deop")));
+            }
+        }
+    }
     else
         message(room, QString("I don't understand '%1'.").arg(l.command));
 }
